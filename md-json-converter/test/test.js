f2ce2f25afd99520accd0935683c9d38bfb6294d
austinibele <92176003+austinibele@users.noreply.github.com>
2024-04-14 04:11:07 -0400

    Clean up tests

--- md-json-converter/test/test.js
+++ md-json-converter/test/test.js
@@ -9,22 +9,33 @@ import cleanData from './clean_json.json' assert { type: 'json' };
 // Initialize markdown variable
 let markdown;
 
-/////// Test md2json
-// Read the mock markdown file
+// ------------------------------------------------------------------------------------------------------
+// Test MD to clean JSON
+// ------------------------------------------------------------------------------------------------------
 const markdownContent = fs.readFileSync(new URL('../test/markdown_text.md', import.meta.url), 'utf-8');
-// Test converter on markdown text
-const result = md2cleanjson(markdownContent);
-// Log the output for visual inspection
-console.log(result);
+let result = md2cleanjson(markdownContent);
+result = JSON.parse(result);
+console.assert(result.blocks.length === 22, 'ERROR! MD to clean JSON failed!!!!!!!');
 
-/////// Test json2md
+
+// ------------------------------------------------------------------------------------------------------
+// Test JSON to MD
+// ------------------------------------------------------------------------------------------------------
 markdown = json2md(data);
-console.log(markdown);
+console.assert(markdown.length === 9294, 'ERROR! JSON to MD failed!!!!!!!');
 
-/////// Test json2cleanjson
+
+// ------------------------------------------------------------------------------------------------------
+// Test JSON to clean JSON
+// ------------------------------------------------------------------------------------------------------
 const cleanJSON = json2cleanjson(data);
-console.log(JSON.stringify(cleanJSON, null, 2));
+console.assert('titleBlocks' in cleanJSON, 'ERROR! titleBlocks not present in cleanJSON');
+console.assert('bodyBlocks' in cleanJSON, 'ERROR! bodyBlocks not present in cleanJSON');
+console.assert(cleanJSON.titleBlocks.blocks.length === 2, 'ERROR! titleBlocks length is not 2');
+console.assert(cleanJSON.bodyBlocks.blocks.length === 30, 'ERROR! bodyBlocks length is not 30');
 
-/////// Test cleanjson2md
+// ------------------------------------------------------------------------------------------------------
+// Test Clean JSON to MD
+// ------------------------------------------------------------------------------------------------------
 markdown = cleanjson2md(cleanData);
-console.log(markdown);
+console.assert(markdown.length === 3470, 'ERROR! Clean JSON to MD failed!!!!!!!');
